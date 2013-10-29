1733eaf1945eb0eb9fa43e04a649f5a2cef383cb
Matthew Butler <butler.matthew@gmail.com>
2013-10-29 10:14:56 -0300

    Changes for Polymer 0.8.7
    * Removed polymer/init.dart script.
    * Calling initPolymer() from main.
    * Moved router initialization to _init() which has the @initMethod
    attribute from Polymer.
    * Added moustache's to event callbacks on-click

--- web/routes_example.dart
+++ web/routes_example.dart
@@ -1,8 +1,9 @@
-//library routes_example;
+library routes_example;
 
 import 'dart:html';
-import 'package:polymer/polymer.dart';
+//import 'dart:async'; // Needed if we use the scheduleMicrotask
 import 'package:route/client.dart';
+import 'package:polymer/polymer.dart';
 
 // Alternatively we could define multiple static routes as such:
 // new UrlPattern('/routes_example/web/routes_example.html#one');
@@ -10,19 +11,26 @@ import 'package:route/client.dart';
 final MyUrl = new UrlPattern(r'/routes_example/web/routes_example.html#(\w+)');
 final HomeUrl = new UrlPattern(r'/routes_example/web/routes_example.html');
 
-// This is temorary only until Polymer 0.8.6 and I should then be able to
-// revert this back to main and drop the @initMethod annotation.
-//@initMethod
-//tmpMain() {
-// After Polymer 0.8.6 use the following two lines instead.
 main() {
-  //initPolymer();
+  // Dartium only allows one dart script per page. So we need to manually
+  // call initPolymer ourselves.
+  initPolymer(); // Start polymer
+
+}
+
+// Indicate to polymer this should be run in a dirtCheckZone
+// Basically tells polymer to watch for changes to observables that may
+// be triggered by this function.
+// Alternative is to keep this in the main method and ensure when we update
+// the observable we also call Observable.dirtyCheck in the event loop.
+@initMethod 
+void _init() {
   // Create our router and handlers then start listening.
   // See route package documentation for more details.
   var router = new Router()..addHandler(MyUrl, route_handler)
       ..addHandler(HomeUrl, (_) { })
       ..listen();
-  
+    
 }
 
 void route_handler(String path) {
@@ -31,6 +39,8 @@ void route_handler(String path) {
   var page = MyUrl.parse(path)[0];
   
   // Grab our custom element and assign to the page property.
-  var exampleApp = document.query('example-app');
+  var exampleApp = document.querySelector('example-app');
   exampleApp.page = page;
+  // Because we're using an @initMethod above we don't need to do this
+//  scheduleMicrotask(Observable.dirtyCheck);
 }
